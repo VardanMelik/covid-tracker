4eebd4402cfcddc054a541415a4be6826bf4d6d9
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-02-02 15:38:39 +0400

    update

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LineGraph.css';
-//import { Line } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 
 function LineGraph() {
+    conts [data, setData] = useState();
     return (
         <div className="lineGraph">
-            
+            <Line data options />
         </div>
     )
 }
